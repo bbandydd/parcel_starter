6b0d55ae071c3eacd51348d251514fed9d073d63
Andy Tsai <bbandydd@gmail.com>
2018-03-21 00:45:09 +0800

    first commit

---
+++ src/js/main.ts
@@ -0,0 +1,5 @@
+import '../css/main.scss';
+import utils from './utils.ts';
+
+console.log('hello world');
+utils.show();
---
+++ src/js/utils.ts
@@ -0,0 +1,5 @@
+export default {
+    show: () => {
+        console.log('utils show');
+    }
+}
